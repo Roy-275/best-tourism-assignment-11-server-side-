50f122a30b48e97e133ed2695bc567e637377359
Roy-275 <anikroy275@gmail.com>
2021-10-31 13:26:56 +0600

    assignment done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 
 
 const app = express();
@@ -44,10 +45,47 @@ async function run() {
             res.send(orders);
         })
 
+        //GET API for single order
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await allOrders.findOne(query)
+            res.send(order);
+        })
+
         //POST API orders
         app.post('/orders', async (req, res) => {
             const newOrder = req.body;
             const result = await allOrders.insertOne(newOrder)
+            res.json(result);
+        })
+
+        //UPDATE API
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedStatus = req.body;
+            console.log(req.body)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: updatedStatus.status
+                },
+            };
+
+            const result = await allOrders.updateOne(filter, updateDoc, options)
+            console.log('updating order status', result)
+
+            res.json(result);
+        })
+
+        //Delete API orders
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+
+            const result = await allOrders.deleteOne(query);
+
             res.json(result);
         })
     }
